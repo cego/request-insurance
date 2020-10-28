07318e66b5d63cc4f4d8d1a3b3ab1c3b02c39d2a
niza <niza@cego.dk>
2020-10-28 12:58:02 +0100

    Rename

--- publishable/migrations/2020_10_27_120537_add_performance_indexes.php
+++ publishable/migrations/2020_10_27_120537_add_performance_indexes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddForeignIndexToRequestInsuranceLogsTable extends Migration
+class AddPerformanceIndexes extends Migration
 {
     /**
      * Run the migrations.
