623f36790bb83595821493f7d52f300cf23555bd
Thomas Wogelius <thwo@cego.dk>
2021-01-20 13:59:49 +0100

    Moved routes to top

--- publishable/routes/web.php
+++ publishable/routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Cego\RequestInsurance\Controllers')->prefix('vendor')->group(function () {
+    Route::get('request-insurances/load', [
+        'uses' => 'RequestInsuranceController@load',
+        'as'   => 'request-insurances.load',
+    ])->middleware('api');
+
+    Route::get('request-insurances/monitor', [
+        'uses' => 'RequestInsuranceController@monitor',
+        'as'   => 'request-insurances.monitor',
+    ])->middleware('web');
+
     Route::resource('request-insurances', 'RequestInsuranceController')
         ->only(['index', 'show', 'destroy'])
         ->middleware('web');
@@ -16,14 +26,4 @@ Route::namespace('Cego\RequestInsurance\Controllers')->prefix('vendor')->group(f
         'uses' => 'RequestInsuranceController@unlock',
         'as'   => 'request-insurances.unlock',
     ])->middleware('web');
-
-    Route::get('request-insurances/load', [
-        'uses' => 'RequestInsuranceController@load',
-        'as'   => 'request-insurances.load',
-    ])->middleware('api');
-
-    Route::get('request-insurances/monitor', [
-        'uses' => 'RequestInsuranceController@monitor',
-        'as'   => 'request-insurances.monitor',
-    ])->middleware('api');
 });
