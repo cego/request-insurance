66f4f1fe3c86c2e50b8f42c4c357b82541d663d3
Thomas Wogelius <thwo@cego.dk>
2021-01-29 13:38:48 +0100

    Bugfixes

--- publishable/migrations/2021_01_29_121827_set_default_value_for_payload_request_insurance.php
+++ publishable/migrations/2021_01_29_121827_set_default_value_for_payload_request_insurance.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIndicesRequestInsurance extends Migration
+class SetDefaultValueForPayloadRequestInsurance extends Migration
 {
     /**
      * Run the migrations.
