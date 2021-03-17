b7ee01943e785fc61e43a17e16b3355c71f9a4ff
niza <niza@cego.dk>
2021-03-17 22:30:34 +0100

    Added priority enum

---
+++ src/RequestInsurance/Enums/Priority.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cego\RequestInsurance\Enums;
+
+abstract class Priority
+{
+    public const DEFAULT = 9999;
+
+    public const VERY_LOW = 9000;
+    public const LOW = 7000;
+    public const MEDIUM = 5000;
+    public const HIGH = 3000;
+    public const VERY_HIGH = 1000;
+}
