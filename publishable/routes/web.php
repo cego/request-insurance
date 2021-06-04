98c3505e441d724e0038e43ea1f38116b4bbaae2
Thomas Wogelius <thwo@cego.dk>
2021-06-04 14:03:18 +0200

    Removed csrf

--- publishable/routes/web.php
+++ publishable/routes/web.php
@@ -8,22 +8,29 @@ Route::namespace('Cego\RequestInsurance\Controllers')->prefix('vendor')->group(f
         'as'   => 'request-insurances.load',
     ])->middleware('api');
 
-    Route::get('request-insurances/monitor', [
-        'uses' => 'RequestInsuranceController@monitor',
-        'as'   => 'request-insurances.monitor',
-    ])->middleware('api');
+    Route::group(function() {
+        Route::get('request-insurances/monitor', [
+            'uses' => 'RequestInsuranceController@monitor',
+            'as'   => 'request-insurances.monitor',
+        ]);
 
-    Route::resource('request-insurances', 'RequestInsuranceController')
-        ->only(['index', 'show', 'destroy'])
-        ->middleware('web');
+        Route::resource('request-insurances', 'RequestInsuranceController')
+            ->only(['index', 'show', 'destroy']);
 
-    Route::post('request-insurances/{request_insurance}/retry', [
-        'uses' => 'RequestInsuranceController@retry',
-        'as'   => 'request-insurances.retry',
-    ])->middleware('web');
+        Route::post('request-insurances/{request_insurance}/retry', [
+            'uses' => 'RequestInsuranceController@retry',
+            'as'   => 'request-insurances.retry',
+        ]);
 
-    Route::post('request-insurances/{request_insurance}/unlock', [
-        'uses' => 'RequestInsuranceController@unlock',
-        'as'   => 'request-insurances.unlock',
-    ])->middleware('web');
+        Route::post('request-insurances/{request_insurance}/unlock', [
+            'uses' => 'RequestInsuranceController@unlock',
+            'as'   => 'request-insurances.unlock',
+        ]);
+    })->middleware([
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ]);
 });
