913fc1a65559cec97711fcb1f6fcf6f246b8a94e
Thomas Wogelius <thwo@cego.dk>
2021-06-07 10:50:18 +0200

    Better routes

--- publishable/routes/web.php
+++ publishable/routes/web.php
@@ -1,36 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\VerifyCsrfToken;
 
-Route::namespace('Cego\RequestInsurance\Controllers')->prefix('vendor')->group(function () {
-    Route::get('request-insurances/load', [
-        'uses' => 'RequestInsuranceController@load',
-        'as'   => 'request-insurances.load',
-    ])->middleware('api');
+Route::namespace('Cego\RequestInsurance\Controllers')
+    ->prefix('vendor')
+    ->middleware('api')
+    ->group(function () {
+        Route::get('request-insurances/load', [
+            'uses' => 'RequestInsuranceController@load',
+            'as'   => 'request-insurances.load',
+        ]);
 
-    Route::group(function() {
         Route::get('request-insurances/monitor', [
             'uses' => 'RequestInsuranceController@monitor',
             'as'   => 'request-insurances.monitor',
         ]);
+    });
 
+Route::namespace('Cego\RequestInsurance\Controllers')
+    ->prefix('vendor')
+    ->middleware('web')
+    ->group(function () {
         Route::resource('request-insurances', 'RequestInsuranceController')
-            ->only(['index', 'show', 'destroy']);
+            ->only(['index', 'show', 'destroy'])
+            ->withoutMiddleware(VerifyCsrfToken::class);
 
         Route::post('request-insurances/{request_insurance}/retry', [
             'uses' => 'RequestInsuranceController@retry',
             'as'   => 'request-insurances.retry',
-        ]);
+        ])->withoutMiddleware(VerifyCsrfToken::class);
 
         Route::post('request-insurances/{request_insurance}/unlock', [
             'uses' => 'RequestInsuranceController@unlock',
             'as'   => 'request-insurances.unlock',
-        ]);
-    })->middleware([
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
-});
+        ])->withoutMiddleware(VerifyCsrfToken::class);
+    });
