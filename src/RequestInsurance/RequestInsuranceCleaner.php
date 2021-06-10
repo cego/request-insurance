b9088a9f32119a2735240ba9a7f4ea63c4b4b9cc
niza <niza@cego.dk>
2021-06-10 12:07:21 +0200

    Made DEDC a happy tiger

--- src/RequestInsurance/RequestInsuranceCleaner.php
+++ src/RequestInsurance/RequestInsuranceCleaner.php
@@ -4,6 +4,7 @@ namespace Cego\RequestInsurance;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
 use Cego\RequestInsurance\Models\RequestInsurance;
 use Cego\RequestInsurance\Models\RequestInsuranceLog;
@@ -19,17 +20,24 @@ class RequestInsuranceCleaner
             $daysToKeepRecords = Carbon::now()->subDays(Config::get('requestInsurance.cleanUpKeepDays', 14));
 
             // Get RI ids to delete
+            /** @var Collection $idsToDelete */
             $idsToDelete = resolve(RequestInsurance::class)::whereBetween('response_code', [200, 299])
                 ->where('created_at', '<', $daysToKeepRecords)
                 ->forUpdate()
                 ->get(['id'])
                 ->pluck('id');
 
-            // Clean up RequestInsurances table
-            resolve(RequestInsurance::class)->whereIn('id', $idsToDelete)->delete();
+            // Chunk delete RI rows
+            $idsToDelete->chunk(1000)->each(function (Collection $idChunk) {
+                // Clean up RequestInsurances table
+                resolve(RequestInsurance::class)->whereIn('id', $idChunk)->delete();
 
-            // Clean up RequestInsuranceLogs table
-            resolve(RequestInsuranceLog::class)::whereIn('request_insurance_id', $idsToDelete)->delete();
+                // Clean up RequestInsuranceLogs table
+                resolve(RequestInsuranceLog::class)::whereIn('request_insurance_id', $idChunk)->delete();
+
+                // Be a good boy and sleep 10ms
+                usleep(10000);
+            });
         });
     }
 }
