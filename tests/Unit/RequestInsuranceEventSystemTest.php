8f5ae63683ceee40d3bd865bc937b0be1dece70e
Niki Ewald Zakariassen <niza@cego.dk>
2021-09-06 15:58:46 +0200

    Added tests for before processing event

--- tests/Unit/RequestInsuranceEventSystemTest.php
+++ tests/Unit/RequestInsuranceEventSystemTest.php
@@ -3,15 +3,19 @@
 namespace Tests\Unit;
 
 use Exception;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Config;
 use Cego\RequestInsurance\Events\RequestFailed;
 use Cego\RequestInsurance\Mocks\MockCurlRequest;
+use Cego\RequestInsurance\RequestInsuranceWorker;
+use Illuminate\Http\Client\Events\RequestSending;
 use Cego\RequestInsurance\Models\RequestInsurance;
 use Cego\RequestInsurance\Events\RequestSuccessful;
 use Cego\RequestInsurance\Events\RequestServerError;
 use Cego\RequestInsurance\Events\RequestClientError;
+use Cego\RequestInsurance\Events\RequestBeforeProcess;
 use Cego\RequestInsurance\Exceptions\EmptyPropertyException;
 
 class RequestInsuranceEventSystemTest extends TestCase
@@ -119,5 +123,107 @@ class RequestInsuranceEventSystemTest extends TestCase
         // Assert
         Event::assertNotDispatched(RequestClientError::class);
         Event::assertDispatched(RequestServerError::class);
+    }
+
+    /** @test */
+    public function it_can_abandon_requests_before_they_are_sent(): void
+    {
+        // Arrange
+        Event::listen(function (RequestBeforeProcess $event) {
+            $event->requestInsurance->abandon();
+        });
+
+        $requestInsurance = RequestInsurance::getBuilder()
+            ->url('https://test.lupinsdev.dk')
+            ->method('get')
+            ->create();
+
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNull($requestInsurance->paused_at);
+        $this->assertNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNull($requestInsurance->abandoned_at);
+
+        // Act
+        $requestInsurance->process();
+
+        // Assert
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNull($requestInsurance->paused_at);
+        $this->assertNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNotNull($requestInsurance->abandoned_at);
+    }
+
+    /** @test */
+    public function it_can_complete_requests_before_they_are_sent(): void
+    {
+        // Arrange
+        Event::listen(function (RequestBeforeProcess $event) {
+            $event->requestInsurance->update(['completed_at' => Carbon::now()]);
+        });
+
+        $requestInsurance = RequestInsurance::getBuilder()
+            ->url('https://test.lupinsdev.dk')
+            ->method('get')
+            ->create();
+
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNull($requestInsurance->paused_at);
+        $this->assertNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNull($requestInsurance->abandoned_at);
+
+        // Act
+        $requestInsurance->process();
+
+        // Assert
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNull($requestInsurance->paused_at);
+        $this->assertNotNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNull($requestInsurance->abandoned_at);
+    }
+
+    /** @test */
+    public function it_can_pause_requests_before_they_are_sent(): void
+    {
+        // Arrange
+        Event::listen(function (RequestBeforeProcess $event) {
+            $event->requestInsurance->pause();
+        });
+
+        $requestInsurance = RequestInsurance::getBuilder()
+            ->url('https://test.lupinsdev.dk')
+            ->method('get')
+            ->create();
+
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNull($requestInsurance->paused_at);
+        $this->assertNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNull($requestInsurance->abandoned_at);
+
+        // Act
+        $requestInsurance->process();
+
+        // Assert
+        $requestInsurance->refresh();
+
+        $this->assertEquals(0, $requestInsurance->retry_count);
+        $this->assertNotNull($requestInsurance->paused_at);
+        $this->assertNull($requestInsurance->completed_at);
+        $this->assertNull($requestInsurance->locked_at);
+        $this->assertNull($requestInsurance->abandoned_at);
     }
 }
