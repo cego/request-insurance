32f70f015d007e93f0b21a18f30988261eb0e6ab
Niki Ewald Zakariassen <niza@cego.dk>
2022-05-06 16:22:19 +0200

    Almost binary search

--- src/RequestInsurance/RequestInsuranceWorker.php
+++ src/RequestInsurance/RequestInsuranceWorker.php
@@ -177,7 +177,7 @@ class RequestInsuranceWorker
 
         $this->memDebug('END:   FETCH RI');
 
-        $this->memDebug('#### Sleeping before processing 3333333333333333333');
+        $this->memDebug('#### Sleeping before processing 4444444444444');
         sleep(5);
 
         $requests->each(function ($request) {
