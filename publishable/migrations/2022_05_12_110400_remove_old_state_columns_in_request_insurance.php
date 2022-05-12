0f07a9f0e10d267cd23210e928659eab1bd110f1
Niki Ewald Zakariassen <niza@cego.dk>
2022-05-12 11:06:37 +0200

    Added missing migration

---
+++ publishable/migrations/2022_05_12_110400_remove_old_state_columns_in_request_insurance.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Cego\RequestInsurance\Enums\State;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveOldStateColumnsInRequestInsurance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->dropColumn('completed_at');
+            $table->dropColumn('paused_at');
+            $table->dropColumn('locked_at');
+            $table->dropColumn('abandoned_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->timestamp('completed_at')->nullable()->default(null);
+            $table->timestamp('abandoned_at')->nullable()->default(null);
+            $table->timestamp('locked_at')->nullable()->default(null);
+            $table->timestamp('paused_at')->nullable()->default(null);
+
+            $table->index(['paused_at', 'abandoned_at', 'completed_at', 'locked_at', 'retry_at', 'priority'], 'covering_index');  // Worker row locking query + Active Rows + Failed Rows
+            $table->index(['completed_at', 'created_at']);                                                                        // Interface search - created_at
+            $table->index(['abandoned_at', 'created_at']);                                                                        // Interface search - abandoned_at
+        });
+
+    }
+}
