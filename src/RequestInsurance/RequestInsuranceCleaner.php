f38abe742ec5ed20624b8a1e1ea1692988da4f41
Niki Ewald Zakariassen <niza@cego.dk>
2022-05-12 10:47:02 +0200

    Updated clean up script to look at new states

--- src/RequestInsurance/RequestInsuranceCleaner.php
+++ src/RequestInsurance/RequestInsuranceCleaner.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\Config;
+use Cego\RequestInsurance\Enums\State;
 use Cego\RequestInsurance\Models\RequestInsurance;
 use Cego\RequestInsurance\Models\RequestInsuranceLog;
 
@@ -25,7 +26,8 @@ class RequestInsuranceCleaner
 
         // Delete RI that have been completed for more than cleanUpKeepDays
         $query->select(['id'])
-            ->where('completed_at', '<', $deletionCutoff)
+            ->whereIn('state', [State::COMPLETED, State::ABANDONED])
+            ->where('state_changed_at', '<', $deletionCutoff)
             ->chunkById($chunkSize, fn (Collection $idsToDelete) => static::deleteChunk($idsToDelete->pluck('id')));
     }
 
