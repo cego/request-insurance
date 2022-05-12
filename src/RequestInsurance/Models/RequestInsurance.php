c6be466f7c220465c786058f830bc8ede3cb666f
Niki Ewald Zakariassen <niza@cego.dk>
2022-05-12 11:28:28 +0200

    Fixed retry at update bug

--- src/RequestInsurance/Models/RequestInsurance.php
+++ src/RequestInsurance/Models/RequestInsurance.php
@@ -643,6 +643,10 @@ class RequestInsurance extends SaveRetryingModel
 
         $this->state = $state;
         $this->state_changed_at = Carbon::now();
+
+        if ($state != State::WAITING) {
+            $this->retry_at = null;
+        }
     }
 
     /**
