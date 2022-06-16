d56bd211f8f7ba42330cd67e8431014867a3a186
Jacob Bjørklund <jabj@cego.dk>
2022-06-16 12:34:26 +0200

    Removed TODO

--- src/RequestInsurance/Controllers/RequestInsuranceEditController.php
+++ src/RequestInsurance/Controllers/RequestInsuranceEditController.php
@@ -29,7 +29,6 @@ class RequestInsuranceEditController extends Controller
 
         RequestInsuranceEdit::create([
             'request_insurance_id' => $requestInsurance->id,
-            // TODO should the required_amount_of_approvals be read from config (default is 1 in migration)
             'old_priority'         => $requestInsurance->priority,
             'new_priority'         => $requestInsurance->priority,
             'old_url'              => $requestInsurance->url,
