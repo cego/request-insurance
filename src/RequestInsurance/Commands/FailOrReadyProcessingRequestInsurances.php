9b115e152fccc09c3b08ab52be797a1a921ad4e4
gusb <gusb@cego.dk>
2022-10-26 16:20:18 +0200

    Made it so it only logs if it actually updated a request insurance

--- src/RequestInsurance/Commands/FailOrReadyProcessingRequestInsurances.php
+++ src/RequestInsurance/Commands/FailOrReadyProcessingRequestInsurances.php
@@ -55,11 +55,13 @@ class FailOrReadyProcessingRequestInsurances extends Command
         // Get ids of request insurances that have been updated, so they can be included in the log.
         $ids = $reqs->get("id");
 
-        // Update state depending on retries_inconsistent
-        $stateChange = $retries_inconsistent ? State::READY : State::FAILED;
-        $reqs->update(["state" => $stateChange]);
+        if ( ! $ids->isEmpty()) {
+            // Update state depending on retries_inconsistent
+            $stateChange = $retries_inconsistent ? State::READY : State::FAILED;
+            $reqs->update(["state" => $stateChange]);
 
-        $boolString = $retries_inconsistent ? "true" : "false";
-        Log::info(print("Request insurances with ids $ids, with retry_inconsistent = $boolString, were set to state $stateChange, due to processing for too long."));
+            $boolString = $retries_inconsistent ? "true" : "false";
+            Log::info(print("Request insurances with ids $ids, with retry_inconsistent = $boolString, were set to state $stateChange, due to processing for too long."));
+        }
     }
 }
