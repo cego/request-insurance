b65c91e140028f073ef6faccb9380aa1eaef0f4b
gusb <gusb@cego.dk>
2022-12-22 15:03:38 +0100

    removed unnecessary line of code

--- src/RequestInsurance/Commands/FailOrReadyProcessingRequestInsurances.php
+++ src/RequestInsurance/Commands/FailOrReadyProcessingRequestInsurances.php
@@ -3,7 +3,6 @@
 namespace Cego\RequestInsurance\Commands;
 
 use Carbon\Carbon;
-use Cego\RequestInsurance\ViewComponents\HttpCode;
 use Illuminate\Console\Command;
 use Cego\RequestInsurance\Enums\State;
 use Cego\RequestInsurance\Models\RequestInsurance;
