1df18fea183af881ca957e312f9d81e77b514bff
gusb <gusb@cego.dk>
2023-01-05 17:06:23 +0100

    quick check

--- src/RequestInsurance/AsyncRequests/RequestPool.php
+++ src/RequestInsurance/AsyncRequests/RequestPool.php
@@ -79,7 +79,10 @@ class RequestPool
             'headers'     => array_merge($requestInsurance->getHeadersCastToArray(), ['User-Agent' => 'RequestInsurance']),
             'body'        => $requestInsurance->payload,
             'timeout'     => $requestInsurance->getEffectiveTimeout(),
-            'on_stats'    => fn (TransferStats $stats) => $requestInsurance->setTimings($stats),
+            //'on_stats'    => fn (TransferStats $stats) => $requestInsurance->setTimings($stats),
+            'on_stats'      => function (TransferStats $stats) use($requestInsurance) {
+                $requestInsurance->setTimings($stats);
+            },
             'http_errors' => false,
         ]);
     }
