0d2604fafa10d8ba4a945274727ca6a1c2a9842a
Mads Jon Nielsen <mjn@cego.dk>
2023-02-24 11:11:55 +0100

    Drop redundant indexes

---
+++ publishable/migrations/2023_02_24_105700_drop_redundant_request_insurances_indexes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropRedundantRequestInsurancesIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->dropIndex('request_insurances_abandoned_at_created_at_index');
+            $table->dropIndex('request_insurances_completed_at_created_at_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->index('created_at', 'request_insurances_abandoned_at_created_at_index');
+            $table->index('created_at', 'request_insurances_completed_at_created_at_index');
+        });
+    }
+}
