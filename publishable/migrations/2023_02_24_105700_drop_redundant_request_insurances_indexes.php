6deeacb4e60cb789de1def0676e72e93d6f47784
Niki Zakariassen <niza@cego.dk>
2024-01-26 10:41:47 +0100

    Test commit

--- publishable/migrations/2023_02_24_105700_drop_redundant_request_insurances_indexes.php
+++ publishable/migrations/2023_02_24_105700_drop_redundant_request_insurances_indexes.php
@@ -14,8 +14,8 @@ class DropRedundantRequestInsurancesIndexes extends Migration
     public function up(): void
     {
         Schema::table('request_insurances', function (Blueprint $table) {
-            $table->dropIndex('request_insurances_abandoned_at_created_at_index');
-            $table->dropIndex('request_insurances_completed_at_created_at_index');
+//            $table->dropIndex('request_insurances_abandoned_at_created_at_index');
+//            $table->dropIndex('request_insurances_completed_at_created_at_index');
         });
     }
 
