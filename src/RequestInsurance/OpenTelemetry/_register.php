413a8473b956cf84f5d7ce84c74243281d2949a6
Lau Josefsen <lejo@cego.dk>
2024-01-29 10:07:25 +0100

    Do not throw if otel is not loaded

--- src/RequestInsurance/OpenTelemetry/_register.php
+++ src/RequestInsurance/OpenTelemetry/_register.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 use OpenTelemetry\SDK\Sdk;
 use Cego\RequestInsurance\OpenTelemetry\RequestInsuranceInstrumentation;
 
-if ( ! class_exists(Sdk::class) || Sdk::isInstrumentationDisabled(RequestInsuranceInstrumentation::NAME) === true) {
+if (class_exists(Sdk::class) && Sdk::isInstrumentationDisabled(RequestInsuranceInstrumentation::NAME) === true) {
     return;
 }
 
 if (extension_loaded('opentelemetry') === false) {
-    trigger_error('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry Cego request-insurance auto-instrumentation', E_USER_WARNING);
-
     return;
 }
 
