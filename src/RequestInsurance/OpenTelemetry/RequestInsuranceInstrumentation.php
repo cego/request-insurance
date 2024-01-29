eea47aaef56724e6aedccec76dcfdabe9c36bb24
Lau Josefsen <lejo@cego.dk>
2024-01-29 09:46:24 +0100

    Doc and refactor

--- src/RequestInsurance/OpenTelemetry/RequestInsuranceInstrumentation.php
+++ src/RequestInsurance/OpenTelemetry/RequestInsuranceInstrumentation.php
@@ -17,6 +17,16 @@ class RequestInsuranceInstrumentation
 {
     public const NAME = 'cego-request-insurance';
 
+    /**
+     * Instruments the given class method, and gives the span around it the given spanName.
+     *
+     * @param CachedInstrumentation $instrumentation
+     * @param string $className
+     * @param string $methodName
+     * @param string $spanName
+     *
+     * @return void
+     */
     private static function hookClassMethod(CachedInstrumentation $instrumentation, string $className, string $methodName, string $spanName): void
     {
         hook(
@@ -27,11 +37,32 @@ class RequestInsuranceInstrumentation
                 Context::storage()->attach($span->storeInContext(Context::getCurrent()));
             },
             static function ($object, $params, $result, ?Throwable $exception) {
-                self::endCurrentSpan($exception);
+                $scope = Context::storage()->scope();
+
+                if ( ! $scope) {
+                    return;
+                }
+
+                $scope->detach();
+                $span = Span::fromContext($scope->context());
+
+                if ($exception) {
+                    $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
+                    $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+                } else {
+                    $span->setStatus(StatusCode::STATUS_OK);
+                }
+
+                $span->end();
             }
         );
     }
 
+    /**
+     * Initiated by ./_register.php
+     *
+     * @return void
+     */
     public static function register(): void
     {
         $instrumentation = new CachedInstrumentation('dk.cego.request-insurance-instrumentation');
@@ -44,31 +75,5 @@ class RequestInsuranceInstrumentation
         foreach ($methodsToHook as $methodToHook) {
             self::hookClassMethod($instrumentation, $methodToHook[0], $methodToHook[1], $methodToHook[2]);
         }
-    }
-
-    /**
-     * @param Throwable|null $exception
-     *
-     * @return void
-     */
-    private static function endCurrentSpan(?Throwable $exception): void
-    {
-        $scope = Context::storage()->scope();
-
-        if ( ! $scope) {
-            return;
-        }
-
-        $scope->detach();
-        $span = Span::fromContext($scope->context());
-
-        if ($exception) {
-            $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
-            $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
-        } else {
-            $span->setStatus(StatusCode::STATUS_OK);
-        }
-
-        $span->end();
     }
 }
