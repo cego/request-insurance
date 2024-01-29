0ded21b65a25d045c57d910143f9c4d81074e0fd
Lau Josefsen <lejo@cego.dk>
2024-01-29 09:52:48 +0100

    Do not error if otel is not installed

--- src/RequestInsurance/OpenTelemetry/_register.php
+++ src/RequestInsurance/OpenTelemetry/_register.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use OpenTelemetry\SDK\Sdk;
 use Cego\RequestInsurance\OpenTelemetry\RequestInsuranceInstrumentation;
 
-if (class_exists(Sdk::class) && Sdk::isInstrumentationDisabled(RequestInsuranceInstrumentation::NAME) === true) {
+if ( ! class_exists(Sdk::class) || Sdk::isInstrumentationDisabled(RequestInsuranceInstrumentation::NAME) === true) {
     return;
 }
 
