dac4518d355165ca2e8f4acbfd0e253cf8384b20
jakobkollerup <128644226+jakobkollerup@users.noreply.github.com>
2025-02-25 15:20:20 +0100

    Feat: Migrate url to be of type text rather than string, to allow for long urls. (#121)
    * Changes for allowing long urls

---
+++ publishable/migrations/2025_02_25_143000_edit_url_length_request_insurances.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditUrlLengthRequestInsurances extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->dropIndex(['url', 'created_at']);
+            $table->text('url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('request_insurances', function (Blueprint $table) {
+            $table->string('url')->change();
+            $table->index(['url', 'created_at']);
+        });
+    }
+};
