375db9aaac4213508f25b8f7ed48d339ec075566
Lau Ernebjerg Josefsen <44977457+LauJosefsen@users.noreply.github.com>
2026-08-31 12:26:32 +0200

    Correct the Tailwind browser build SRI hash (#175)

--- publishable/views/layouts/master.blade.php
+++ publishable/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
         applyColorScheme(colorScheme);
         colorScheme.addEventListener('change', applyColorScheme);
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss-browser/4.3.2/index.global.min.js" integrity="sha512-wf0Z7EtuTFwP09V3hWIeyfzBKDlqjvwls1kjiGH8F2xB6hwD04IXyOyCADqlIyr+HtwgAq7CtPMll7eI49Ab8Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss-browser/4.3.2/index.global.min.js" integrity="sha512-zGa8OZQnDW3gAFAZxYlhn2UfSEKvU2M1UXwbKzw7OB4ZyQJ6qPf9Lt1BQwZ3ZsbW5YycTkSgitvHfgbCWPUt4Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <style type="text/tailwindcss">
         @custom-variant dark (&:where(.dark, .dark *));
 
